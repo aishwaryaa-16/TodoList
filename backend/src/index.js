fcc4317b124a5a17f9acdb4a4594664705280608
Aishwarya Muralidhar <aishwarya.murali1603@gmail.com>
2025-03-18 16:20:26 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+dotenv.config()
+
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    })
+)
+app.use("/api/auth", authRoutes)
+
+app.listen(5000,()=>{
+    console.log("server is running on port 5000")
+    connectDB()
+})
+
