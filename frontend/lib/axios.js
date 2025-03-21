6ebbbdf59acdf25cb4aef7361048f7406fb0e6b8
Aishwarya Muralidhar <aishwarya.murali1603@gmail.com>
2025-03-21 17:24:10 +0530

    minor fixes

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:5000/api" ,  //base url for the api-common part for all the api ROUTES
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
 });
